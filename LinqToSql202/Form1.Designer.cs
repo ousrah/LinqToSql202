0964eef350cacc87ab30c8b23948a6c61b299044
rahmouni oussama <ousrah@hotmail.com>
2022-03-09 10:04:52 +0100

    Ajoutez des fichiers projet.

---
+++ LinqToSql202/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace LinqToSql202
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(433, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(806, 672);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(743, 696);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(414, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Liste des editeurs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(13, 379);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(414, 34);
+            this.button11.TabIndex = 3;
+            this.button11.Text = "distinct";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(13, 299);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(414, 34);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "Inner join";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(414, 34);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "variables order by Methode";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(11, 339);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(414, 34);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "passer a une sous table";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(11, 259);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(414, 34);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "jointure produit cartésien";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(414, 34);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "nom et villes des editeurs de paris";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 219);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(414, 34);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "order by descending Expression linq";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(414, 34);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "noms et villes des editeurs";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(13, 179);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(414, 34);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "order by descending methode";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 724);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ LinqToSql202/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqToSql202
+{
+    public partial class Form1 : Form
+    {
+        librairieDataContext db = new librairieDataContext();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+               var ed = db.EDITEUR.Where(c => c.VILLEED=="paris" );
+         //   var ed = from edi in db.EDITEUR
+         //            where edi.VILLEED == "paris"
+         //            select edi;
+
+            label1.Text = ed.Count().ToString();
+
+
+
+
+            dataGridView1.DataSource = ed;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+         
+            //afficher la liste des noms et des villes de tous les editeurs
+            var ed = from edi in db.EDITEUR
+                     select new { nom = edi.NOMED, ville = edi.VILLEED };
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+
+            //afficher la liste des noms et des villes des editeurs qui habitent a paris
+
+            //avec ==
+            //var ed = from edi in db.EDITEUR
+            //         where edi.VILLEED == "paris"
+            //         select new { edi.NOMED, edi.VILLEED };
+
+            //avec equals
+            var ed = from edi in db.EDITEUR
+                     where edi.VILLEED.Equals("PARIS")
+                     select new { edi.NOMED, edi.VILLEED };
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+
+            //afficher la liste des noms et des villes des editeurs qui habitent a paris
+            //avec le tri descendant sur les nom
+
+            var x = db.EDITEUR;
+            var a = x.Where(c => c.VILLEED == "paris");
+            var b = a.OrderBy(c => c.NOMED); //OrderByDescending
+            var ed = b.Select(c => new { c.NOMED, c.VILLEED });
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            var ed = db.EDITEUR
+                .Where(c => c.VILLEED == "paris")
+                .OrderByDescending(c => c.NOMED)
+                .Select(c => new { c.NOMED, c.VILLEED });
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+
+            var ed = from edi in db.EDITEUR
+                     where (edi.VILLEED == "paris")
+                     orderby edi.NOMED //ascending  //descending
+                     select new { edi.NOMED, edi.VILLEED };
+
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+
+
+            // liste des editeurs et des ouvrages
+
+
+            //produit cartesien
+            var ed = (from edi in db.EDITEUR
+                      from ouv in db.OUVRAGE
+                    where edi.NOMED == ouv.NOMED
+                      select new { edi.NOMED, ouv.NOMOUVR }).Distinct();
+
+            label1.Text = ed.Count().ToString();
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+
+            //jointure interne (inner join)
+            var ed = (from edi in db.EDITEUR
+                      join ouv in db.OUVRAGE
+                      on edi.NOMED equals ouv.NOMED
+                      select new { edi.NOMED, ouv.NOMOUVR }).Distinct();
+            label1.Text = ed.Count().ToString();
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+
+            // passer à une table sans jointure en utilisant la clé étrangère
+
+            var ed = from ouv in db.OUVRAGE
+
+                     select new { ouv.EDITEUR.VILLEED, ouv.NOMOUVR };
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            //appliquer distinct sur le resultat de linq
+
+            var ed = (from edi in db.EDITEUR
+                      select new { edi.NOMED }).Distinct();
+            label1.Text = ed.Count().ToString();
+
+
+            dataGridView1.DataSource = ed;
+        }
+
+
+
+    }
+}
